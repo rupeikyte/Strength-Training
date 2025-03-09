f553d67ab2a9749cedfbb9a7813fcb423b6903a0
rupeikyte <157068263+rupeikyte@users.noreply.github.com>
2025-03-09 14:15:51 -0500

    started setting up inheritance for excercises

---
+++ Strength Training/Excercise Types/Legs.swift
@@ -0,0 +1,11 @@
+//
+//  Legs.swift
+//  Strength Training
+//
+//  Created by Rūta Rupeikytė on 3/9/25.
+//
+
+class Legs: Excercise {
+    // subclass definition goes here
+}
+
---
+++ Strength Training/Excercise.swift
@@ -0,0 +1,16 @@
+//
+//  Excercise.swift
+//  Strength Training
+//
+//  Created by Rūta Rupeikytė on 3/9/25.
+//
+
+class Excercise {
+    var type = ""
+    var description: String {
+        return "The workout is named at \(type)."
+    }
+    func getTime() {
+    }
+}
+
