8ff09331356f3ce7a53fb2448555551d2f4fff4a
rupeikyte <157068263+rupeikyte@users.noreply.github.com>
2025-03-10 16:08:50 -0500

    build does not fail

--- Strength Training/Excercise Types/Legs.swift
+++ Strength Training/Excercise Types/Legs.swift
@@ -5,19 +5,19 @@
 //  Created by Rūta Rupeikytė on 3/9/25.
 //
 
-class Legs: Excercise {
-    
-    override init(type: String, time: Double) {
-        super.init(type: String, time: Double)
-    }
-    
-    override func getTime() -> Double {
-        return time
-    }
-    
-    override func getType() -> String {
-        return type
-    }
-
-}
+//class Legs: Excercise {
+//    
+//    override init(type: String, time: Double) {
+//        super.init(type: String, time: Double)
+//    }
+//    
+//    override func getTime() -> Double {
+//        return time
+//    }
+//    
+//    override func getType() -> String {
+//        return type
+//    }
+//
+//}
 
