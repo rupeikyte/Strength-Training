73c0dab67d2acb7803cc5a41c6de5857fccc30bb
nraehl <nraehl@macalester.edu>
2025-03-27 16:06:47 -0500

    View Updates

--- Strength Training/ContentView.swift
+++ Strength Training/ContentView.swift
@@ -13,14 +13,12 @@ import SwiftData
 
 //var getDayInformation = Day
 
-//@State var workoutInfo: [String] = []
 
 
 struct ContentView: View {
-//    @StateObject var workoutInfo = workouts()
-
+    
     @State var workoutInfo: [String] = []
-
+    
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
     let array: [Int] = Array(1...35)
@@ -28,101 +26,45 @@ struct ContentView: View {
     @State private var fgColor: Color = .gray
     
     let months: [String] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
+    
     
     var body: some View {
-
-        
-        ZStack {
-            VStack {
+        NavigationStack{
+            VStack{
                 Text("Schedule")
                     .font(.title)
-                VStack {
-                    Text("insert month var here")
-                    allDays
-                }
-            }
-        }
-    }
-    //assign exercises between days
-    
-    
-    var workoutText: some View {
-        VStack{
-            ForEach(0..<self.workoutInfo.count, id: \.self) { index in
-                Text(self.workoutInfo[index])
-            }
-            .font(.title)
-        }
-    }
-    
-    var buttons: some View {
-        HStack{
-            Button("Arm") {
-                workoutInfo.append("armInfo")
-            }
-            Button("Shoulder") {
-                workoutInfo.append("shoulderInfo")
-            }
-            Button("Leg") {
-                workoutInfo.append("legInfo")
-            }
-            Button("Back") {
-                workoutInfo.append("backInfo")
-            }
-            Button("Chest") {
-                workoutInfo.append("chestInfo")
-            }
-            Button("Abs") {
-                workoutInfo.append("absInfo")
-            }
-        }
-    }
-    
-    
-    var allDays: some View {
-        NavigationStack {
-            ForEach(1..<6) { index1 in
-                HStack(spacing: 10) {
-                    ForEach(1..<8) { index2 in
-                        NavigationLink(destination:  {
-                            buttons
-                                .font(.title)
-                            workoutText
-                        }, label: {
-                            DayView(day: (index2)+(7*(index1-1)))
-                        })
-                    }
-                }
-            }
+                MonthView()
+                            }
         }
     }
 }
-    
-//    
+
+
+
+//
 //struct DayView: View {
-//    
+//
 //    @State private var fgColor: Color = .gray
 //    @State private var tall: CGFloat = 100
-//    
+//
 //    let day: Int
-//    
-//    
+//
+//
 ////    @StateObject var workoutInfo = workouts()
-//    
+//
 //    var body: some View {
-//        
+//
 //        VStack {
 //            Text("\(day)")
 ////            Text(workoutInfo.dayWorkouts[day])
 //            Text("test")
 //        }
-//        
+//
 //        .padding()
 //        .background(fgColor)
 //        .frame(width: tall, height: 100)
 //        .cornerRadius(5)
-//        
+//
 //    }
 //}
 //
@@ -130,9 +72,9 @@ struct ContentView: View {
 //////    @StateObject var workoutInfo = workouts()
 //////    cancellable = workoutInfo.objectWillChange
 //////        .sink { _ in
-//////        
+//////
 //////    }
-////    
+////
 ////    var body: some View {
 ////        @StateObject var workoutInfo = workouts()
 ////
@@ -152,13 +94,14 @@ struct ContentView: View {
 //
 //
 ////struct workout {
-////    
+////
 ////}
 ////
 ////struct schedule {
-////    
+////
 ////}
 //
+
 //#Preview {
 //    ContentView()
 //        .modelContainer(for: Item.self, inMemory: true)
