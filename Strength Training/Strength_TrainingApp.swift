674bd90547f5ba2e37b6b8d2368f0a54df64ce8e
rupeikyte <157068263+rupeikyte@users.noreply.github.com>
2025-04-09 16:10:03 -0500

    pseudo code for algorithm uploaded

--- Strength Training/Strength_TrainingApp.swift
+++ Strength Training/Strength_TrainingApp.swift
@@ -15,18 +15,31 @@ struct Strength_TrainingApp: App {
             Item.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
+        
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
     }()
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
         .modelContainer(sharedModelContainer)
     }
+    
+    
+    // mystery algorithm
+    
+    //takes in a month of workouts
+    
+    // for day in month
+    // if day.hasWorkout
+    //   check if day has <=  3 workouts
+    //   for (2 consecutive days){check if the muscle group is not touched again} if false, error
+    //   for (3){check if the next three days .hasWorkout}  if true, error
+    //   for (6){check if muscle group hit again} if not, error
+    //  check all muscle groups that have been hit in this day (putIfAbsent like in maps)
 }
